6bc70fb0f2f95a339f680ec354047ecaa4e0fc16
andrey <devZhulanov.A.A@gmail.com>
2020-10-20 15:39:16 +0500

    Add CrossOrigin from host http://localhost:3000 to fix #1

--- src/main/java/com/psu/lab_app/QuestionController.java
+++ src/main/java/com/psu/lab_app/QuestionController.java
@@ -2,6 +2,7 @@ package com.psu.lab_app;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ public class QuestionController {
         this.repository = repository;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/questions")
     List<Question> all() {
         return repository.findAll();
